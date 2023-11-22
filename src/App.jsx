d182232f675e1edb0eabb1bf80921b5517919e6c
Alexedpx <alexandradepourtoux93@gmail.com>
2023-11-22 11:11:53 +0100

    création de la todolist

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./style/index.scss";
+import Todo from "./components/Todo.jsx";
+
+
+function App() {
+ 
+  return (
+    <>
+    <Todo />
+   
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Todo.jsx
@@ -0,0 +1,13 @@
+export default function Todo() {
+	return (
+		<>
+			<div className="Todo-wrapper">
+				<h1>To Do</h1>
+
+				<div className="add-wrapper">
+					<input className="input" type="text" placeholder="Add a task"/>
+				</div>
+			</div>
+		</>
+	);
+}
